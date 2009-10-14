c3c3360077df877eeda36ab88b7c28e740012961
sun-sentinel <sun-sentinel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-14 18:36:23 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sun-sentinel-florida-marlins-news-blogs-widget/trunk@163435 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sun-sentinel-marlins.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Plugin Name: Sun-Sentinel Marlins News and Blog Widget
+Plugin URI: http://www.sun-sentinel.com/
+Description: Sun-Sentinel Marlins News and Blog Widget
+Author: Kit Mobley
+Version: 1.0
+Author URI: http://www.sun-sentinel.com/
+*/
+  
+			
+function ssmarlins_widget() {
+	  ?>
+
+<link rel="stylesheet" href="http://www.sun-sentinel2.com/widgets/tabber_marlins/example.css" TYPE="text/css" MEDIA="screen">
+
+<script type="text/javascript">document.write('<style type="text/css">.tabber{display:none;}<\/style>');</script>
+
+<style type="text/css">
+<!--
+#getwidget {
+float: right; font-size: 9px; color: #FFFFFF; margin-right: 5px;
+}
+#getwidget a {
+color:#FFFFFF;
+}
+
+-->
+</style>
+ <script type="text/javascript" src="http://www.sun-sentinel2.com/widgets/tabber/tabber.js"></script>
+ 
+<div style="width: 300px; height: 250px; background-color:#000000;">
+<div style="width: 300px; background-image: url(http://www.sun-sentinel2.com/widgets/tabber_marlins/images/top_bg.jpg); height: 74px;">
+<div id="getwidget"><a href="http://www.sun-sentinel.com/sfl-rss-widgets,0,6223346.htmlstory" target="_blank">Get this widget</a></div>
+</div>
+<div class="tabber">
+     <div class="tabbertab" style="background-color:#FFFFFF; margin-left: 2px;">
+	  <h2>Florida Marlins News</h2>
+	   <?php
+$a = file_get_contents("http://www.sun-sentinel2.com/widgets/rss2html.php?XMLFILE=http://www.sun-sentinel.com/sports/florida-marlins/rss2.0.xml&TEMPLATE=http://www.sun-sentinel2.com/widgets/sample-template-marlins-news.html&MAXITEMS=4");
+echo ($a);
+?>
+     </div>
+<div class="tabbertab" style="background-color:#FFFFFF; margin-left: 2px;">
+	  <h2>Florida Marlins Blog</h2>
+	  <?php
+$b = file_get_contents("http://www.sun-sentinel2.com/widgets/rss2html.php?XMLFILE=http://blogs.sun-sentinel.com/sports_baseball_marlins/index.rdf&TEMPLATE=http://www.sun-sentinel2.com/widgets/sample-template-marlins-blogs.html&MAXITEMS=4");
+echo ($b);
+?>
+     </div>
+
+    
+</div>
+</div>
+<?php
+      }
+      function init_ssmarlins(){
+          register_sidebar_widget("Sun-Sentinel Marlins News and Blog Widget", "ssmarlins_widget");    
+      }
+      add_action("plugins_loaded", "init_ssmarlins");
+?>
